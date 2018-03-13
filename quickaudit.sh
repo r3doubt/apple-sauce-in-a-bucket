2ddb213c31029a2464fbf19c6b6d7c115f1c5102
R3doubt <32580761+r3doubt@users.noreply.github.com>
2018-03-13 07:54:28 -0400

    Update quickaudit.sh

--- quickaudit.sh
+++ quickaudit.sh
@@ -6,13 +6,13 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 #audit list of setuid and setgid for third-party binaries
-echo "List of setuid and setgid files" >> /Users/admin/Desktop/quickAudit.txt
-find / -perm -4000 -o -perm -2000 -type f >> /Users/admin/Desktop/quickAudit.txt 2>/dev/null
+echo "List of setuid and setgid files" >> /Users/[user]/Desktop/quickAudit.txt
+find / -perm -4000 -o -perm -2000 -type f >> /Users/[user]/Desktop/quickAudit.txt 2>/dev/null
 
 #quick list of services scheduled to run from launchd
-echo "List of Launchd processes" >> /Users/admin/Desktop/quickAudit.txt
-launchctl list >> /Users/admin/Desktop/quickAudit.txt
+echo "List of Launchd processes" >> /Users/[user]/Desktop/quickAudit.txt
+launchctl list >> /Users/[user]/Desktop/quickAudit.txt
 
 #check your loginwindow plist (or any other plist)
-echo "loginwindow plist contents" >> /Users/admin/Desktop/quickAudit.txt
-/usr/libexec/PlistBuddy -c "Print" com.apple.loginwindow.plist >> /Users/admin/Desktop/quickAudit.txt
+echo "loginwindow plist contents" >> /Users/[user]/Desktop/quickAudit.txt
+/usr/libexec/PlistBuddy -c "Print" com.apple.loginwindow.plist >> /Users/[user]/Desktop/quickAudit.txt
